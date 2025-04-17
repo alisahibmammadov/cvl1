2fb1a7d5da31bfbab4fde7f9a20b10cab8181151
alisahibmammadov <ali.yek.10@mail.ru>
2025-04-17 20:24:16 +0400

    bg photo change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
 import videoBg from "./assets/bgvideo.mp4"; 
+import Logo from "./assets/Logo.png"; 
 import Links from "./components/Links";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app">
-      <video autoPlay loop muted className="video-bg">
+      {/* <video autoPlay loop muted className="video-bg">
         <source src={videoBg} type="video/mp4" />
-      </video>
+      </video> */}
+      <img src={Logo} alt="" className="video-bg" />
+
 
       <div className="content flex flex-col gap-5 md:gap-8">
         <Navbar />
