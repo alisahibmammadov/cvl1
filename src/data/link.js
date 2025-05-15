f926785cf196574aed72c040b21a08b22c021938
alisahibmammadov <ali.yek.10@mail.ru>
2025-05-15 19:44:32 +0400

    cvl

--- src/data/link.js
+++ src/data/link.js
@@ -1,22 +1,22 @@
 const LinkData = [
-  // {
-  //   id: 1,
-  //   link: "https://music.yandex.com/users/nphne-5kmuxbrm/playlists/1000?utm_medium=copy_link",
-  //   linkName: "Yandex Playlist",
-  //   linkIcon:"fa-yandex"
-  // },
-  // {
-  //   id: 2,
-  //   link: "https://music.apple.com/az/playlist/searchmusic/pl.u-06oxpqNCWADj5yz",
-  //   linkName: "Apple Music",
-  //   linkIcon:"fa-itunes-note"
-  // },
-  // {
-  //   id: 3,
-  //   link: "https://open.spotify.com/user/31btadld7eft7lid2hvi3zidu624?si=9kwwNSc9Ta6GzLc60zJJnw",
-  //   linkName: "Spotify Playlist",
-  //   linkIcon:"fa-spotify"
-  // },
+  {
+    id: 1,
+    link: "https://music.yandex.com/users/nphne-5kmuxbrm/playlists/1000?utm_medium=copy_link",
+    linkName: "Yandex Playlist",
+    linkIcon:"fa-yandex"
+  },
+  {
+    id: 2,
+    link: "https://music.apple.com/az/playlist/searchmusic/pl.u-06oxpqNCWADj5yz",
+    linkName: "Apple Music",
+    linkIcon:"fa-itunes-note"
+  },
+  {
+    id: 3,
+    link: "https://open.spotify.com/user/31btadld7eft7lid2hvi3zidu624?si=9kwwNSc9Ta6GzLc60zJJnw",
+    linkName: "Spotify Playlist",
+    linkIcon:"fa-spotify"
+  },
   // {
   //   id: 4,
   //   link: "https://www.youtube.com/@HarmoniXI",
@@ -29,24 +29,24 @@ const LinkData = [
   //   linkName: "Cavel Records",
   //   linkIcon:"fa-youtube"
   // },
-  {
-    id: 6,
-    link: "https://www.youtube.com/@MelodiXI",
-    linkName: "MelodiXI",
-    linkIcon:"fa-youtube"
-  },
+  // {
+  //   id: 6,
+  //   link: "https://www.youtube.com/@MelodiXI",
+  //   linkName: "MelodiXI",
+  //   linkIcon:"fa-youtube"
+  // },
   // {
   //   id: 7,
   //   link: "https://www.youtube.com/@UtopiXI",
   //   linkName: "UtopiXI",
   //   linkIcon:"fa-youtube"
   // },
-  // {
-  //   id: 8,
-  //   link: "https://t.me/CavelRecords",
-  //   linkName: "Cavel Records",
-  //   linkIcon:"fa-telegram"
-  // },
+  {
+    id: 8,
+    link: "https://t.me/CavelRecords",
+    linkName: "Cavel Records",
+    linkIcon:"fa-telegram"
+  },
   // {
   //   id: 9,
   //   link: "https://t.me/searchmusic1",
